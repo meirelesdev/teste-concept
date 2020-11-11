830ff59fa80410a0d4af38d47fd5158f306c28e5
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-11-11 07:18:50 -0300

    alterações e .htaccess

--- classes/Sql.php
+++ classes/Sql.php
@@ -1,34 +1,12 @@
 <?php
-       ':FIRST_NAME' => $data['first_name'],
-       ':LAST_NAME' => $data['last_name'],
-       ':ZIPCODE' => $data['zipcode'],
-       ':CITY' => $data['city'],
-       ':LOGRADOURO' => $data['logradouro'],
-       ':STATE' => $data['state'],
-       ':NUMBER' => $data['number'],
-       ':COMPLEMENT' => $data['complement'],
-       ':NEIGHBORHOOD' => $data['neighborhood']
 
-CREATE TABLE clients (
-	id serial PRIMARY KEY,
-	first_name VARCHAR ( 50 )  NOT NULL,
-	last_name VARCHAR ( 50 ) NOT NULL,
-	zipcode VARCHAR ( 20 )  NOT NULL,
-	city VARCHAR ( 35 )  NOT NULL,
-	state VARCHAR ( 35 )  NOT NULL,
-	logradouro VARCHAR ( 35 ) ,
-    number INT NOT NULL,
-	complement VARCHAR ( 35 )  NOT NULL,
-	neighborhood VARCHAR ( 35 )  NOT NULL   
-	 
-);
 class Sql {
 
     private $conn;
 
     public function __construct(){
         
-        $this->conn =  new PDO("pgsql:dbname=d1hf7g37o8vo8h;host=ec2-54-165-164-38.compute-1.amazonaws.com", "ketfherckcfehg", "453549c83c19c54970b1ccc252adc8bd343cfc102f7a87209cfdd72784f54af8");
+        $this->conn =  new PDO("pgsql:dbname=meudb;host=localhost", "root", "root");
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
